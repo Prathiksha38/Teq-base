7fcfcd0947d4e9e3c6b33ca0aecc456732fca027
Prathiksha <125107901+Prathiksha38@users.noreply.github.com>
2025-06-26 16:35:38 +0530

    Update db_con.py

--- emp/db_con.py
+++ emp/db_con.py
@@ -1,8 +1,19 @@
+# import pymongo
+# # from pymongo import MongoClient
+# con = pymongo.MongoClient("mongodb+srv://prathiksha383:OibBzIDTvnI2XIk8@cluster3.gqobdtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3")
+# db = con ['project'] 
+
+# # import mongoengine
+
+# # db=mongoengine.connect('project')  
+
 import pymongo
-# from pymongo import MongoClient
-con = pymongo.MongoClient("mongodb+srv://prathiksha383:OibBzIDTvnI2XIk8@cluster3.gqobdtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3")
-db = con ['project'] 
 
-# import mongoengine
+# ✅ Updated connection string with tls=true to fix Render SSL error
+con = pymongo.MongoClient(
+    "mongodb+srv://prathiksha383:OibBzIDTvnI2XIk8@cluster3.gqobdtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3&tls=true"
+)
 
-# db=mongoengine.connect('project')  
+# ✅ Connect to the 'project' database
+db = con['project']
+
