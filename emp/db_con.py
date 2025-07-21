261acb8565e9cbfcc6cf5014f37c3826fb86867f
Prathiksha <125107901+Prathiksha38@users.noreply.github.com>
2025-07-21 18:42:14 +0530

    Update db_con.py

--- emp/db_con.py
+++ emp/db_con.py
@@ -3,7 +3,7 @@ import pymongo
 import certifi
 
 # ✅ MongoDB Atlas connection string
-MONGO_URI = "mongodb+srv://prathiksha383:prathiksha383@cluster0.khqiya2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://prathiksha383:prathiksha383@cluster0.khqiya2.mongodb.net/project?retryWrites=true&w=majority&appName=Cluster0"
 
 # ✅ Connect using certificate
 con = pymongo.MongoClient(MONGO_URI, tlsCAFile=certifi.where())
